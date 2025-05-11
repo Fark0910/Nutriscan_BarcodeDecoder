306741b566840a2428906ae08d3a5a9a89c60964
Fark0910 <fardeen7781@gmail.com>
2025-05-11 23:18:51 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 # main.py
-from fastapi import FastAPI, File, UploadFile
+
+from fastapi import FastAPI, File, UploadFile,HTTPException
 from fastapi.responses import JSONResponse
 from dynamsoft_barcode_reader_bundle import *
 import cv2
@@ -8,7 +9,7 @@ import numpy as np
 from PIL import Image
 import os
 from dotenv import load_dotenv
-
+import requests 
 
 load_dotenv()  # Load variables from .env
 license_key = os.getenv("DYNAMSOFT_LICENSE_KEY")
@@ -37,11 +38,27 @@ def decode_image(image_data):
     return result_json
 
 @app.post("/decode")
-async def decode(file: UploadFile = File(...)):
-    content = await file.read()
-    np_arr = np.frombuffer(content, np.uint8)
-    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+async def decode(file: UploadFile = File(...)): 
+    try:
+        content = await file.read()
+        np_arr = np.frombuffer(content, np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    result = decode_image(image)
-    return JSONResponse(content=result)
+        if image is None:
+            raise HTTPException(status_code=400, detail="Image decoding failed")
+
+       
+        try:
+            requests.post("https://nutriscan-backend-api.onrender.com/fastapires", json={"message": "image under processing"})
+        except Exception as express_error:
+            print("Could not reach Express server:", express_error)
+
+        result = decode_image(image)
+        return JSONResponse(content=result)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
 
---
+++ pins.h
@@ -0,0 +1,8 @@
+// pins.h
+#ifndef PINS_H
+#define PINS_H
+
+#define GREEN_LED_PIN 4
+#define RED_LED_PIN 2
+
+#endif
